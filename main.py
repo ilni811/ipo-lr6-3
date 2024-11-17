e4d64424b472603b4363bb1a42a58a13dfddd450
starybog <nikitapishchalin@gmail.com>
2024-11-17 14:49:41 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("я негр черный")
+str1 = int (input("введите колво строк "))
+if str1 < 1:
+    print("введено не верно")
+else:
+    # ввод строк
+    list = []
+    for i in range(str1):
+        list.append(str(input(f"Введите вашу строку {i + 1} ")))
+        # обьединение строк с пробелами
+        list_join = " ".join(list)
+        # разделение строки на слова
+        list_join = list_join.split()
+        # длина исходной строки
+        list_kolvo = len(list_join)
+        # количество различных слов 
+        list_negr = len(set(list_join))
+print(f"различных слов {list_negr}, слов всего {list_kolvo}")
+     
